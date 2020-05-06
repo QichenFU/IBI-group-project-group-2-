b5a07e672ea0481fb9c2d907c53bdb8dac60fc13
kk007007 <61218080+kk007007@users.noreply.github.com>
2020-05-06 13:39:51 +0800

    add task 4 code

---
+++ Code/Task 4.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 16:41:56 2020
+
+@author: 蒋冰
+"""
+import re
+import sys
+#define the function
+def mRNA_protein(RNA_string):
+    #check if the input sequence is a correct RNA sequence, if not exit the function and give an error message
+    if re.search(r'[^AUCGacug]',RNA_string):
+        print("not a correct RNA sequence") 
+        sys.exit()
+    #change lower case to upper case
+    RNA_string=RNA_string.upper()
+    #input the genetic code dictionary
+    start_code = 'AUG'
+    protein_table = {'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V', 
+                     'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V', 
+                     'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V', 
+                     'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+                     'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A', 
+                     'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A', 
+                     'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A', 
+                     'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A', 
+                     'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D', 
+                     'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D', 
+                     'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E', 
+                     'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E', 
+                     'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G', 
+                     'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G', 
+                     'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+                     'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'}
+    #find start codon
+    start_sit = re.search(start_code, RNA_string)
+    protein = ''
+    #if sart codon exist, translate until we meet the stop codon
+    if RNA_string.find('AUG') > -1:
+        for sit in range(start_sit.end() - 3, len(RNA_string), 3):
+            if protein_table[RNA_string[sit:sit+3]] == 'Stop':
+                break
+            else:
+                protein = protein + protein_table[RNA_string[sit:sit+3]]
+        print('peptide translated from  mRNA is :',protein)
+    # cannot find start codon
+    else:
+        print('cannot find start codon')
+    #output the result
+
+RNA_string ='CGCGCGCGAUGCUUAUUGUGGCCGCGGAUAACUAAGGC'
+mRNA_protein(RNA_string)
+    
+
+
+
+
+
+
+
+
+
+
